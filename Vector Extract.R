09b3e58754e50243ad220b05556d541b00b46de6
Praneeth Kumar. S <114051847+pandupraneeth@users.noreply.github.com>
2023-06-20 11:42:19 +0530

    Create Vector Extract.R

---
+++ Vector Extract.R
@@ -0,0 +1,29 @@
+# Create a numeric vector of length 10
+
+numeric_vector <- c(1.5, 2.3, 4.6, 7.8, 9.0, 10.1, 12.3, 15.6, 18.9, 20.0)
+
+print(numeric_vector)
+
+# Create a complex vector of length 10
+
+complex_vector <- c(1+2i, 2+3i, 3+4i, 4+5i, 5+6i, 6+7i, 7+8i, 8+9i, 9+10i, 10+11i)
+
+print(complex_vector)
+
+# Create a logical vector of length 10
+
+logical_vector <- c(TRUE, FALSE, TRUE, TRUE, FALSE, FALSE, TRUE, TRUE, TRUE, FALSE)
+
+print(logical_vector)
+
+# Create a character vector of length 10
+
+character_vector <- c("apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "imbe", "jackfruit")
+
+print(character_vector)
+
+# Extract values at positions 2, 3, 4, and 5 from the numeric vector
+
+selected_values <- numeric_vector[2:5]
+
+print(selected_values)
